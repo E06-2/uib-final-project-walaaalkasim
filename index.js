cb51fa5867600714782ef92915a3c6bdd9655457
walaa.alkasim <walaa.alkasim@hotmail.com>
2022-05-08 23:54:54 +0200

    js file

---
+++ index.js
@@ -0,0 +1,41 @@
+function openTab(infoTab) {
+  var i;
+  var x = document.getElementsByClassName("info-tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(infoTab).style.display = "block";
+}
+
+let percentNode = document.getElementById("percent-node");
+let countNode = 0;
+setInterval(() => {
+  if (countNode === 70) {
+    clearInterval();
+  } else {
+    countNode += 1;
+    percentNode.innerHTML = countNode + "%";
+  }
+}, 27);
+
+let percentMongo = document.getElementById("percent-mongo");
+let countMongo = 0;
+setInterval(() => {
+  if (countMongo === 65) {
+    clearInterval();
+  } else {
+    countMongo += 1;
+    percentMongo.innerHTML = countMongo + "%";
+  }
+}, 29);
+
+let percentTeam = document.getElementById("percent-team");
+let countTeam = 0;
+setInterval(() => {
+  if (countTeam === 85) {
+    clearInterval();
+  } else {
+    countTeam += 1;
+    percentTeam.innerHTML = countTeam + "%";
+  }
+}, 23);
